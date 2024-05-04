8ac84b2ef5f0c2faafe64d47e1787cb78a080b81
Jaron Singley <jaron_c_sharp@hotmail.com>
2024-05-04 10:32:43 -0400

    Added EntityFramework and Created Database

---
+++ Data/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public virtual IList<Hotel> Hotels { get; set; } //Use this for one to many relationship, can have multiple things
+    }
+}
